c7751a5e2705f00a44f221dcaba4ac3462fea210
Rick Hambrook <rick@thespaceinbetween.co.nz>
2015-11-10 10:34:40 +1300

    feat(config): add configs for whitelist/blacklist for sections by handle (closes #2)

--- EntryTitleEditablePlugin.php
+++ EntryTitleEditablePlugin.php
@@ -2,8 +2,8 @@
 
 /**
  *
- * @package    AssetLinks
- * @version    Version 1.0
+ * @package    EntryTitleEditable
+ * @version    Version 1.1
  * @author     Rick Hambrook
  * @copyright  Copyright (c) 2015
  * @link       www.rickhambrook.com
@@ -18,7 +18,7 @@ class EntryTitleEditablePlugin extends BasePlugin {
 	}
 
 	public function getVersion() {
-		return '1.0';
+		return '1.1';
 	}
 
 	public function getDeveloper() {
@@ -35,12 +35,30 @@ class EntryTitleEditablePlugin extends BasePlugin {
 
 	function init() {
 		if (
-			craft()->request->isCpRequest() &&
-			craft()->userSession->isLoggedIn() &&
-			craft()->request->getSegment(1) == "entries"
+			!craft()->request->isCpRequest() ||
+			!craft()->userSession->isLoggedIn() ||
+			craft()->request->getSegment(1) !== "entries" ||
+			!($section = craft()->request->getSegment(2))
 		) {
-			craft()->templates->includeJsResource("entrytitleeditable/js/entrytitle.js");
-			craft()->templates->includeCssResource("entrytitleeditable/css/entrytitle.css");
+			return;
 		}
+
+		// Get black and white lists as arrays
+		$blacklist = craft()->config->get("blacklist", "entrytitleeditable");
+		$blacklist = (is_array($blacklist)) ? $blacklist : [];
+		$whitelist = craft()->config->get("whitelist", "entrytitleeditable");
+		$whitelist = (is_array($whitelist)) ? $whitelist : [];
+
+		// Entries must be in the whitelist, if set. Or not on the blacklist
+		if (
+			($whitelist && !in_array($section, $whitelist)) ||
+			($blacklist && in_array($section, $blacklist))
+		) {
+			return;
+		}
+
+		// Do the thing
+		craft()->templates->includeJsResource("entrytitleeditable/js/entrytitle.js");
+		craft()->templates->includeCssResource("entrytitleeditable/css/entrytitle.css");
 	}
 }
--- README.md
+++ README.md
@@ -10,6 +10,23 @@ Setup
 
 - Download this repository, rename the folder to 'entrytitleeditable' and upload to craft/plugins directory.
 
+Configuration
+====================
+
+You can configure which entry sections have this enabled using either a blacklist or whitelist in `config/entrytitleeditable.php`. For example
+
+    <?php
+
+    return [
+        "blacklist" => [
+            "blog" // section handle
+        ],
+        "whitelist" => [
+        ]
+    ];
+
+If there are sections in the blacklist, then any section NOT in the blacklist will use the plugin. Or if there are sections in the whitelist, then ONLY sections in the whitelist will get this feature.
+
 Todo
 ====================
 
