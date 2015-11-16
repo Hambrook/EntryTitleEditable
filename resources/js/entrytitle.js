3f51adccaae06df16e75448d570745c2ab060dc5
Rick Hambrook <rick@thespaceinbetween.co.nz>
2015-11-16 14:19:02 +1300

    fix(validation): fix entry field not showing validation errors. Thanks @lindseydiloreto for reporting. Fixes #3

--- resources/js/entrytitle.js
+++ resources/js/entrytitle.js
@@ -1,18 +1,25 @@
 $(function(){
 	var
-		$title        = $("#page-header h1"),
-		$titleWrapper = $("#title-field"),
-		$titleField   = $("#title");
+		$titleWrapper      = $("#page-header"),
+		$title             = $titleWrapper.find("h1"),
+		$titleFieldWrapper = $("#title-field"),
+		$titleField        = $("#title");
 	if (!$titleWrapper.length) { return false; }
 
-	$titleWrapper.hide();
+	$titleFieldWrapper
+		.hide()
+		.find("ul.errors")
+			.insertAfter($title);
 	$title
 		.attr("contenteditable", true)
 		.attr("title", "Click to edit")
 		.on("keypress keyup paste", function(){
-			console.log("foo");
 			$(this).html($(this).text());
 			$titleField.val($(this).text());
 		})
 		.after('<i data-icon="edit"></i>');
+
+	if ($titleWrapper.find("ul.errors li").length) {
+		$title.addClass("error");
+	}
 });
